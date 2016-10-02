03c3eb1ffac6fe622b09a8090d6fb427c560f6d9
fergus <fergus@yahoo.com.tw>
2016-10-03 07:12:06 +0800

    1-3 針對衣服建立漂亮的顯示訊息 notice_message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+	def notice_message
+		# 程式內容可參考
+		# http://kakas-blog.logdown.com/posts/286448-rails101-chapter3-notice-message
+	  alert_types = { notice: :success, alert: :danger }
+
+	  close_button_options = { class: "close", "data-dismiss" => "alert", "aria-hidden" => true }
+	  close_button = content_tag(:button, "×", close_button_options)
+
+	  alerts = flash.map do |type, message|
+	    alert_content = close_button + message
+
+	    # alert_types = { notice: :success, alert: :danger } => { :notice => :success, :alert => :danger }
+	    # "notice".to_sym  => :notice
+	    alert_type = alert_types[type.to_sym] || type
+	    alert_class = "alert alert-#{alert_type} alert-dismissable"
+
+	    content_tag(:div, alert_content, class: alert_class)
+	  end
+
+	  alerts.join("\n").html_safe
+	end
 end
