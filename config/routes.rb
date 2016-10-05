6607b592da7e52f0c5b04c66ed72fdb80f2df19a
kejingtai <fergus.jingtai@gmail.com>
2016-10-05 00:50:20 +0000

    7-1 建立使用者後台 ( 前置作業 )

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,12 @@ Rails.application.routes.draw do
       post :quit
     end
   end
+  
+  namespace :account do
+    resources :groups
+    resources :posts
+  end
+
   root 'groups#index'
 
   # Example of regular route:
